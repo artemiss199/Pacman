84cc0bf5a1c04fec928fa71704cd3edf80204ff4
artemiss199 <artemissdabadi@gmail.com>
2026-08-09 13:41:32 +0330

    ADD[ScoreManager]: we now track scores and can save highscore if oru current score is more than previous highscore saved in .txt file at main folder of the project

---
+++ src/ScoreManager.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class ScoreManager {
+    private int currentScore;
+    private int highScore;
+    private final String SCORE_FILE = "highscore.txt";
+
+    private void loadHighScore() {
+        try (BufferedReader br = new BufferedReader(new FileReader(SCORE_FILE))) {
+            highScore = Integer.parseInt(br.readLine());
+        } catch (Exception e) {
+            highScore = 0; // if we couldn't find score file then highScore is considered zero
+        }
+    }
+
+    public ScoreManager() {
+        this.currentScore = 0;
+        loadHighScore();
+    }
+
+    public void addScore(int points) {
+        currentScore += points;
+    }
+
+    public int getCurrentScore() { return currentScore; }
+
+    public void checkAndSaveHighScore() {
+        if (currentScore > highScore) {
+            highScore = currentScore;
+            try (PrintWriter out = new PrintWriter(new FileWriter(SCORE_FILE))) {
+                out.println(highScore);
+            } catch (IOException e) {
+                System.out.println("Error saving high score.");
+            }
+        }
+    }
+
+
+}
